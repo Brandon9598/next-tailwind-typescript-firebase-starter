ce4dd48e9ed14c9c4a011af01332cf61fc448d64
brandon9598 <brandon9598@gmail.com>
2022-07-24 20:56:39 -0400

    Update navbar to be responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useUser } from "../context/userContext";
 import { auth, signInWithGoogle } from "../utils/firebase/clientApp";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
 	const { user } = useUser();
@@ -15,12 +16,37 @@ const Navbar = () => {
 
 	return (
 		<div className="navbar bg-neutral text-neutral-content">
-			<div className="flex-1">
+			<div className="navbar-start">
 				<a className="btn btn-ghost normal-case text-xl">App Logo</a>
 			</div>
-			<div className="flex-none">
+			<div className="navbar-end flex-none md:hidden">
+				<div className="dropdown">
+					<label tabIndex={0}>
+						<MenuIcon />
+					</label>
+					<ul
+						tabIndex={0}
+						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52  text-neutral"
+					>
+						<li>
+							<a>Link 1</a>
+						</li>
+						<li>
+							<a>Link 2</a>
+						</li>
+						<li>
+							{!user ? (
+								<a onClick={handleLogin}>Login</a>
+							) : (
+								<a onClick={handleLogout}>Logout</a>
+							)}
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="navbar-end hidden md:flex ">
 				<ul className="menu menu-horizontal p-0">
-					<li>
+					<li tabIndex={0}>
 						<a>Link 1</a>
 					</li>
 					<li>
